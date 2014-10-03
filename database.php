0de697e09e8599c9bf0dae093933dc8cf1b84fa3
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-03 00:16:44 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,21 +1,25 @@
 <?php
 
-//define database settings.
-define('DB_HOST', 'localhost');
-define('DB_TYPE', 'mysql');
-define('DB_NAME', 'dbname');
-define('DB_USER', 'dbuser');
-define('DB_PASS', 'dbpass');
+/** 
+ * NOTE: this is procedural style. 
+ * $dbh is the database handler. 
+ * 
+ * make sure you replace 'databasename_here' with whatever database name it is that you are using.
+ */
+
+$db_user = 'db_username';
+$db_pass = 'db_password';
 
 try 
 {
-    $dbh = new PDO('DB_TYPE:host=DB_HOST;dbname=DB_NAME', DB_USER, DB_PASS);
+	$dbh = new PDO('mysql:host=localhost;dbname=databasename_here;charset=utf8', $db_user, $db_pass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES, false );
 } 
 catch(PDOException $e) 
 {
-    echo 'There seems to be a connection error. Please report this to a system admin.';
-    file_put_contents('logs/connection_errors.txt', $e->getMessage().PHP_EOL,FILE_APPEND);
+    echo 'There seems to be a connection error. Please contact a system admin asap to get this fixed.';
+    file_put_contents('connection.errors.txt', $e->getMessage().PHP_EOL,FILE_APPEND);
+    exit;
 }
 
 ?>
