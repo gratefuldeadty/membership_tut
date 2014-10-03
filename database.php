cec86d050e52fe905a0679c6b7e855693649a2ac
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-02 21:28:17 -0400

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,21 @@
 <?php
 
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=tasmith_game', $username, $password);
+//define database settings.
+define('DB_HOST', 'localhost');
+define('DB_TYPE', 'mysql');
+define('DB_NAME', 'dbname');
+define('DB_USER', 'dbuser');
+define('DB_PASS', 'dbpass');
+
+try 
+{
+    $dbh = new PDO('DB_TYPE:host=DB_HOST;dbname=DB_NAME', DB_USER, DB_PASS);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES, false );
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
+} 
+catch(PDOException $e) 
+{
+    echo 'There seems to be a connection error. Please report this to a system admin.';
+    file_put_contents('logs/connection_errors.txt', $e->getMessage().PHP_EOL,FILE_APPEND);
 }
+
+?>
