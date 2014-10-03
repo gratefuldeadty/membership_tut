14efaa6965a772bc5d041c207dd0b4cd5acf7fdb
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-03 01:19:18 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,12 @@
 <?php
 
+/**
+ * Example usage on how to use the login function.
+ */
+
 include 'classes/session.php';
 
-Session::init();
+Session::init(); //our session_start.
 
 include 'classes/users.php';
 
@@ -16,6 +20,39 @@ if (isset($_POST['doLogin']))
 	}
 	else
 	{
-		$error = 'Login failed!';
+		echo 'Login failed!!'
 	}
 }
+
+if (!Session::get('loginToken'))
+{
+	Session::set('loginToken', uniqid()); //if the token is not set, 
+}
+?>
+
+<div align="center">
+	<form method="POST">
+		<table cellpadding="2" cellspacing="0">
+			<tr>
+				<th colspan="2" align="left">Login</th>
+			</tr>
+			<tr>
+				<td style="width:124px;"><label>Username:</label></td>
+				<td><input required type="text" name="username" pattern="[a-zA-Z0-9]{2,64}"></td>
+			</tr>
+			<tr>
+				<td style="width:124px;"><label>Password:</label></td>
+				<td><input required type="password" name="password" pattern=".{4,}" autocomplete="off"></td>
+			</tr>
+			<tr>
+				<td>
+					<input type="hidden" name="ip" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>">
+					<input type="hidden" name="loginToken" value="<?php echo Session::get('loginToken'); ?>">
+					<input type="submit" name="doLogin" value="Login">
+				</td>
+			</tr>
+			<tr>
+			</tr>
+		</table>
+	</form>
+</div>
