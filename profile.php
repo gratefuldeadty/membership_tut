8043bd5e741adb6c7a71a92a6210bbb4c171b7e4
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-03 23:08:14 -0400

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,1 +1,22 @@
 <?php
+
+include 'header.php';
+
+/** This page selects which profile to view using $_GET['id]; */
+ 
+$userid = htmlentities((int)$_GET['id']);
+
+//check to see if profile we are viewing exists.
+if ($users->userData($userid)) == false)
+{
+    Session::set('Error', 'This user does not exist.');
+    $users->displayMessage();
+    exit;
+}
+
+//the profile does exist.
+$userData = $users->userData($userid);
+
+echo 'Displaying the profile of '.$username;
+
+
