8551d7476e230f1f66cd811c3283fe78f499b9c5
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-05 17:39:11 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,4 +1,34 @@
 <?php
 
 
+class Values
+{
+        private $dbh;
+        private $userid;
+        
+        public function __construct(PDO $database)
+        {
+                $this->dbh = $database;
+                $this->userid = Session::get('userid');
+        }
+        
+        /**
+         * Simple way to call a value, fo
+         * @param mixed $value
+         * @return string : bool
+         */
+        public static function user($value)
+        {
+                $user = $this->userData($this->userid);
+                return (!empty($value)) ? $user[$value] : false;
+        }
+        
+        
+        public static 
+        
+        
+}
+
+echo Values::user('userid', $id);
+
 ?>
