1efca13480c27cdf5d0a375ff20234a74ec8fe95
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-04 20:10:57 -0400

    Update bcrypt.php

--- classes/bcrypt.php
+++ classes/bcrypt.php
@@ -11,7 +11,6 @@ class Bcrypt
         self::cost = $cost;
     }
     
-
    /**
      * Create a hash for a password.
      * @param mixed $input
@@ -33,6 +32,27 @@ class Bcrypt
     public static function verifyPass($input, $stored)
     {
         $hash = crypt($input, $stored);
+        
+       if (!self::checkLength($hash, $stored))
+        {
+            return false;
+        }
+        
+        //cheap way to check the lengths
+        if (strlen($input) !== strlen($stored))
+        {
+            return false;
+        }
+        
+        //check to make sure each character matches (ie: match == match)
+        for ($i = 0; $i < strlen($input); ++$i)
+        {
+            if ($input[$i] !== $stored[$i])
+            {
+                return false;
+            }
+            usleep(10);
+        }
         return $hash === $stored;
     }
     
@@ -43,53 +63,7 @@ class Bcrypt
      */
     private static function getSalt()
     {
-        return sprintf('$2y$%02d$%s', self::cost, substr(strtr(base64_encode(self::getBytes()), '+', '.'), 0, 22));
-    }
-    
-
-    /**
-     * Validate the length of the submitted hashed password & the stored password.
-     * @param string $stored
-     * @param string $input
-     * @return bool
-     */
-    private static function checkLength($stored, $input)
-    {
-        $stored .= chr(0);
-        $input .= chr(0);
-        
-        $storedLen = strlen($stored);
-        $inputLen = strlen($input);
-        
-        $result = $storedLen - $inputLen;
-        
-        for ($i = 0; $i < $inputLen; ++$i)
-        {
-            $result |= (ord($stored[$i % $storedLen]) ^ ord($input[$i]));
-        }
-        return $result === true;
-    }
-    
-
-    /**
-     * Validate each seperate character matches - (ie: match === match)
-     * @param string $stored
-     * @param string $input
-     * @return bool
-     */
-    private static function checkChars($stored, $input)
-    {
-        if (self::checkLength($stored, $input) === true)
-        {
-            for ($i = 0; $i < strlen($input); ++$i)
-            {
-                return ($input[$i] === $stored[$i]) ? true : false;
-            }
-        }
-        else
-        {
-            return false;
-        }
+    	return sprintf('$2y$%02d$%s', self::$cost, substr(strtr(base64_encode(self::getBytes()), '+', '.'), 0, 22));
     }
     
 
@@ -97,23 +71,25 @@ class Bcrypt
      * OpenSSL - random generator, this will be used in the getSalt() function.
      * @return string 
      */
-    private static function getBytes()
-    {
-        $bytes = '';
-        if (function_exists('openssl_random_pseudo_bytes') AND (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN')
-        {
-            $bytes = openssl_random_pseudo_bytes(18);
-        }
-        if ($bytes === '' AND is_readable('/dev/urandom') AND ($hrand = @fopen('/dev/urandom', 'rb')) !== FALSE)
-        {
-            $bytes = fread($hrand, 18);
-        }
-        $key = ($bytes === '') ? uniqid(self::pref, true) : '';
-        for ($i = 0; $i < self::cost; ++$i)
-        {
-            $bytes = hash_hmac('sha512', microtime() . $bytes, $key, true)
-            usleep(10);
-        }
+    private static function getBytes() 
+    {	
+	    $bytes = '';
+	    if(function_exists('openssl_random_pseudo_bytes') && (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN')) 
+	    {
+	        $bytes = openssl_random_pseudo_bytes(18);
+	    }
+	 
+	    if($bytes === '' && is_readable('/dev/urandom') && ($hRand = @fopen('/dev/urandom', 'rb')) !== FALSE) 
+	    {
+	        $bytes = fread($hRand, 18);
+	        fclose($hRand);
+	    }
+	    $key = ($bytes === '') ? uniqid(self::$pref, true);
+	    for($i = 0; $i < 12; $i++) 
+	    {
+	        $bytes = hash_hmac('sha512', microtime() . $bytes, $key, true);
+	        usleep(10);
+	    }
         return $bytes;
     }
 }
